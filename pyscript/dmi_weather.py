c46c19b8992ed56059a8cf778e9754b091c410fd
Martin Eberhardt <martin@darkfox.dk>
2024-09-28 00:40:42 +0200

    Reimplement DMI weather report using pyscript

---
+++ pyscript/dmi_weather.py
@@ -0,0 +1,130 @@
+import aiohttp
+from datetime import datetime
+import xmltodict
+
+base_url = "https://dmi.dk/dmidk_byvejrWS/rest/texts/"
+
+
+async def get_element_with_text(element, needle):
+    for key, value in element.items():
+        if needle in key:
+            return value
+    return {}
+
+
+async def get_first_element_not_in(element, needles):
+    for key, value in element.items():
+        if key not in needles:
+            return value
+    return {}
+
+
+@service
+async def get_dmi_weather_report(location_id=None):
+    """yaml
+    name: Get DMI Weather Report
+    description: Get the current DMI weather report
+    args:
+        location_id:
+            description: The location ID to get the weather report for
+            required: false
+            type: string
+    """
+    if not location_id:
+        raise ValueError("Location ID is required")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url + str(location_id)) as response:
+            response.raise_for_status()
+            result = response.json()
+
+            location = result.get('name', '')
+            municipality = result.get('municipality', '')
+
+            regiondata = result.get("regiondata", [{}])[0]
+            product = regiondata.get('products', [{}])[0]
+
+            timestamp_str = product.get('timestamp', '')
+            # What even is this format?? "2024-09-27T20:00:11.377Z[UTC]"
+            timestamp = datetime.strptime(
+                timestamp_str.replace('[UTC]', '+00:00'),
+                '%Y-%m-%dT%H:%M:%S.%fZ%z'
+            ) if timestamp_str else None
+
+            xml = xmltodict.parse(product.get("text", ""))
+            dmi = xml.get('dmi', {})
+            date = dmi.get('dato', {}).get('text', '')
+
+            validity = dmi.get('reggyld', {}).get('text', '')
+            title = get_element_with_text(
+                dmi, 'overskrift'
+            ).get('text', '')
+
+            region_dict = get_element_with_text(
+                dmi, 'region'
+            )
+            region = region_dict.get("@name", "")
+            content = get_first_element_not_in(
+                region_dict,
+                ["@name", "text"]
+            ).get("text", "")
+
+            data = {
+                'location': location,
+                'municipality': municipality,
+                'timestamp': timestamp,
+                'date': date,
+                'validity': validity,
+                'title': title,
+                'region': region,
+                'content': content,
+            }
+
+            event.fire('dmi_weather_report', weather_report=data)
+
+
+@service
+async def get_dmi_severe_weather_warnings(location_id=None):
+    """yaml
+    name: Get DMI Severe Weather Warnings
+    description: Get the current DMI severe weather warnings
+    args:
+        location_id:
+            description: The location ID to get the severe weather warnings for
+            required: false
+            type: string
+    """
+    if not location_id:
+        raise ValueError("Location ID is required")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            base_url + "varsler/geonameid/" + str(location_id)
+        ) as response:
+            response.raise_for_status()
+            result = response.json()
+
+            data = {
+                'name': result.get('name', ''),
+                'area': result.get('area', ''),
+                'day0': result.get('day0', ''),
+                'day1': result.get('day1', ''),
+                'day2': result.get('day2', ''),
+                'day3': result.get('day3', ''),
+                'day4': result.get('day4', ''),
+                'day5': result.get('day5', ''),
+                'vandstand': result.get('vandstand', ''),
+                'favorite': result.get("favorite", ""),
+                'warnings': result.get("warnings", []),
+                'location_warnings': result.get("locationWarnings", []),
+                'actual_warnings': result.get("actualWarnings", []),
+                'fiveday_warnings': result.get("fivedayWarnings", []),
+            }
+
+            for key, value in data.items():
+                if value == 'none' or value == '':
+                    data[key] = None
+
+            data['raw'] = result
+
+            event.fire('dmi_severe_weather_warnings', severe_weather_warnings=data)
